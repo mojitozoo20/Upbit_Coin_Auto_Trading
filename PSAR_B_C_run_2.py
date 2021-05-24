f32b1fc8d3a2ee3c2aee15bd7887342f81fbc08a
manogzoo20 <mangozoo20@gmail.com>
2021-05-24 17:50:43 +0900

    Fixed

--- PSAR_B_C_run_2.py
+++ PSAR_B_C_run_2.py
@@ -121,7 +121,7 @@ class Consumer(threading.Thread):
                     print(f"\t{TICKER} [{datetime.datetime.now()}]")
                     print(f"보유량: {upbit.get_balance_t(self.ticker)}, 보유KRW: {cash},  hold_flag= {hold_flag}, wait_flag= {wait_flag}, signal= {curr.SAR < curr.BBAND_LOWER and curr.senkou_spna_A < curr.BBAND_MIDDLE and curr.senkou_spna_B < curr.BBAND_MIDDLE and curr.close < curr.BBAND_UPPER and curr.senkou_spna_A > curr.senkou_spna_B}")
                     print(f"BBAND: [{int(curr.BBAND_UPPER)} {int(curr.BBAND_MIDDLE)} {int(curr.BBAND_LOWER)}], PSAR: {curr.SAR}, 선행1: {curr.senkou_spna_A}, 선행2: {curr.senkou_spna_B}")
-                    print(f"시가: {price_open}, 구매가: {price_buy}, 누적 수익: {cash - CASH} ({cash / CASH * 100}%)")
+                    print(f"시가: {price_curr}, 구매가: {price_buy}, 누적 수익: {cash - CASH} ({cash / CASH * 100}%)")
                     i = 0
                 i += 1
             except:
